bc1833916683a7a4b5523dff9de8ae15253cc4fc
Fabian <fcastill@tibco-support.com>
2016-08-26 13:47:41 -0500

    Add scripts for docker builds and CI server

---
+++ scripts/release-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+unset SCRIPT_ROOT
+readonly SCRIPT_ROOT=$(
+  unset CDPATH
+  script_root=$(dirname "${BASH_SOURCE}")
+  cd "${script_root}"
+  pwd
+)
+source ${SCRIPT_ROOT}/init.sh
+# Build flogo/flogo-web docker image
+pushd ${BUILD_ROOT}
+docker::build flogo/flogo-cli
+# docker::copy_tag_and_push flogo/flogo-cli pdhar/flogo-cli
+popd
