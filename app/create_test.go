ea1900cf896351f3ff1275dcf33fd90e62b01f95
Miguel Torres <migueltorreslopez@gmail.com>
2017-10-11 12:16:15 -0700

    Add tests for create new

--- app/create_test.go
+++ app/create_test.go
@@ -5,7 +5,70 @@ import (
 	"flag"
 	"github.com/TIBCOSoftware/flogo-cli/cli"
 	"os"
+	"path"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"bytes"
 )
+
+var GOLD_FLOGO_JSON = `{
+  "name": "testApp",
+  "type": "flogo:app",
+  "version": "0.0.1",
+  "description": "My flogo application description",
+  "triggers": [
+    {
+      "id": "my_rest_trigger",
+      "ref": "github.com/TIBCOSoftware/flogo-contrib/trigger/rest",
+      "settings": {
+        "port": "9233"
+      },
+      "handlers": [
+        {
+          "actionId": "my_simple_flow",
+          "settings": {
+            "method": "GET",
+            "path": "/test"
+          }
+        }
+      ]
+    }
+  ],
+  "actions": [
+    {
+      "id": "my_simple_flow",
+      "name": "my simple flow",
+      "ref": "github.com/TIBCOSoftware/flogo-contrib/action/flow",
+      "data": {
+        "flow": {
+          "name": "my simple flow",
+          "attributes": [],
+          "rootTask": {
+            "id": 1,
+            "type": 1,
+            "tasks": [
+              {
+                "id": 2,
+                "type": 1,
+                "activityRef": "github.com/TIBCOSoftware/flogo-contrib/activity/log",
+                "name": "log",
+                "attributes": [
+                  {
+                    "name": "message",
+                    "value": "Simple Log",
+                    "type": "string"
+                  }
+                ]
+              }
+            ],
+            "links": [
+            ]
+          }
+        }
+      }
+    }
+  ]
+}`
 
 type TestEnv struct {
 	currentDir string
@@ -28,12 +91,17 @@ func TestCmdCreate_Exec (t *testing.T) {
 		t.Fatal(err)
 	}
 	defer os.Unsetenv("FLOGO_BUILD_EXPERIMENTAL")
-	testEnv := &TestEnv{currentDir:os.TempDir()}
+	tempDir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testEnv := &TestEnv{currentDir:tempDir}
 	defer testEnv.cleanup()
 	t.Logf("Current dir '%s'", testEnv.currentDir)
 	cmd := &cmdCreate{option: optCreate, currentDir: testEnv.getTestwd}
 	fs := flag.NewFlagSet("test", flag.ExitOnError)
-	args := []string{"testApp"}
+	appName := "testApp"
+	args := []string{appName}
 
 	err = cli.ExecCommand(fs, cmd, args)
 	if err != nil {
@@ -43,9 +111,51 @@ func TestCmdCreate_Exec (t *testing.T) {
 	// Validate the structure
 	currentDir := testEnv.currentDir
 	_, err = os.Stat(currentDir)
+	assert.Nil(t, err, "There should be a folder for temp dir '%s'", currentDir)
+
+	// There should be a folder with app name
+	appFolderPath := path.Join(currentDir, appName)
+	fi, err := os.Stat(appFolderPath)
+	assert.Nil(t, err, "There should be a folder for app name '%s'", appFolderPath)
+	assert.True(t, fi.IsDir(), "There should be a folder for app name '%s'", appFolderPath)
+
+	// There should be a flogo.json file
+	flogojsonPath := path.Join(appFolderPath, fileDescriptor)
+	fi, err = os.Stat(flogojsonPath)
+	assert.Nil(t, err, "There should be a file for flogo json '%s'", flogojsonPath)
+
+	// Compare the flogo.json with GOLD file
+	jsonBytes, err := ioutil.ReadFile(flogojsonPath)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	assert.True(t, bytes.Equal(jsonBytes, []byte(GOLD_FLOGO_JSON)), "Unexpected flogo.json format")
+
+	// There should be a src folder
+	srcPath := path.Join(appFolderPath, "src")
+	fi, err = os.Stat(srcPath)
+	assert.Nil(t, err, "There should be a folder for src '%s'", srcPath)
+
+	// There should be an app folder
+	appSrcFolderPath := path.Join(srcPath, appName)
+	fi, err = os.Stat(appSrcFolderPath)
+	assert.Nil(t, err, "There should be a folder for app name '%s'", appSrcFolderPath)
+	assert.True(t, fi.IsDir(), "There should be a folder for app name '%s'", appSrcFolderPath)
+
+	// There should be a main file
+	mainPath := path.Join(appSrcFolderPath, fileMainGo)
+	fi, err = os.Stat(mainPath)
+	assert.Nil(t, err, "There should be a file for main.go '%s'", mainPath)
+
+	// There should be an imports file
+	importsPath := path.Join(appSrcFolderPath, fileImportsGo)
+	fi, err = os.Stat(importsPath)
+	assert.Nil(t, err, "There should be a file for imports.go '%s'", importsPath)
+
+
+
+
 
 
 }
