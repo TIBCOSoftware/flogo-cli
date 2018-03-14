352cce9f01f0097a3e80a606f09dcfaf792eaa50
Matt Ellis <mellis@Matts-MacBook-Pro-3.local>
2018-03-14 11:42:00 -0600

    updating flogogen trigger to use updated interfaces

--- gen/trigger.go
+++ gen/trigger.go
@@ -49,7 +49,7 @@ var tplTriggerDescriptor = `{
       "value": "default"
     }
   ],
-  "outputs": [
+  "output": [
     {
       "name": "output",
       "type": "string"
@@ -77,12 +77,12 @@ type MyTriggerFactory struct{
 	metadata *trigger.Metadata
 }
 
-//NewFactory create a new Trigger factory
+// NewFactory create a new Trigger factory
 func NewFactory(md *trigger.Metadata) trigger.Factory {
 	return &MyTriggerFactory{metadata:md}
 }
 
-//New Creates a new trigger instance for a given id
+// New Creates a new trigger instance for a given id
 func (t *MyTriggerFactory) New(config *trigger.Config) trigger.Trigger {
 	return &MyTrigger{metadata: t.metadata, config:config}
 }
@@ -90,13 +90,12 @@ func (t *MyTriggerFactory) New(config *trigger.Config) trigger.Trigger {
 // MyTrigger is a stub for your Trigger implementation
 type MyTrigger struct {
 	metadata *trigger.Metadata
-	runner   action.Runner
 	config   *trigger.Config
 }
 
-// Init implements trigger.Trigger.Init
-func (t *MyTrigger) Init(runner action.Runner) {
-	t.runner = runner
+// Initialize implements trigger.Init.Initialize
+func (t *MyTrigger) Initialize(ctx trigger.InitContext) error {
+	return nil
 }
 
 // Metadata implements trigger.Trigger.Metadata
@@ -140,6 +139,10 @@ func getJsonMetadata() string{
 type TestRunner struct {
 }
 
+type initContext struct {
+	handlers []*trigger.Handler
+}
+
 // Run implements action.Runner.Run
 func (tr *TestRunner) Run(context context.Context, action action.Action, uri string, options interface{}) (code int, data interface{}, err error) {
 	return 0, nil, nil
@@ -173,6 +176,7 @@ func TestInit(t *testing.T) {
 
 	runner := &TestRunner{}
 
-	tgr.Init(runner)
+	initCtx := &initContext{handlers: make([]*trigger.Handler, 0, len(config.Handlers))}
+	tgr.Initialize(initCtx)
 }
 `
